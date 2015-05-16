122d5427403f3de2c884adaf799b8d783292d6f4
Ryan Serazin <rps9530@rit.edu>
2015-05-16 13:28:21 -0400

    finished constructor and sizeof function

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-wallhack "1.0.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]])
--- src/unsafe/core.clj
+++ src/unsafe/core.clj
@@ -1,11 +1,23 @@
 (ns unsafe.core)
 
-(defn getUnsafe [] 
-  (let [f (.getDeclaredField sun.misc.Unsafe "theUnsafe")]
-    (.setAccessible f true)
-    (.get f nil)))
+(defn- make-unsafe []
+  (let [constructor (.getDeclaredConstructor sun.misc.Unsafe nil)]
+    (.setAccessible constructor true)
+    (.newInstance constructor nil)))
+
+;todo secure this
+(def the-unsafe (make-unsafe))
 
 (defn sizeof [^java.lang.Object obj] 
-  "returns the shallow (doesn't follow pointers) size of a java object."
-  (let [theUnsafe (getUnsafe)]
-    ))
+  "returns the shallow (doesn't follow pointers & ignores static fields) size 
+  of a java object."
+  (let [max-size (apply max (map #(.objectFieldOffset the-unsafe %) 
+                                 (all-nonstatic-fields (.getClass obj))))]
+  (* 8 (+ 1 (/ max-size 8)))))
+
+(defn- all-nonstatic-fields [^java.lang.Class c]
+  (let [is-static? (filter #(not (java.lang.reflect.Modifier/isStatic (.getModifiers %))))
+        dec-fields (mapcat #(.getDeclaredFields %))
+        not-nil    (take-while #(not (nil? %)))
+        xf (comp not-nil dec-fields is-static?)] 
+     (sequence xf (iterate #(.getSuperclass %) c))))
